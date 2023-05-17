09e48872719fb46dd77e5e602bd26e8121d52fd0
VDentesano <dentesanovalentino@gmail.com>
2023-05-16 23:40:45 -0300

    Ya se puede subir los datos a la db

--- Pagina/agregar.php
+++ Pagina/agregar.php
@@ -1,17 +1,25 @@
 <?php
 include("conn.php");
-
-$nombre_completo = $_POST['nombre_completo']; 
-$edad = $_POST['edad'];  
-$dni = $_POST['dni'];
-$email = $_POST['email'];
-$archivo = $_POST['archivo'];
-
-$sql = "INSERT INTO usuario(id, nombre_completo, edad, dni, email, archivo) VALUES (NULL,'[value-2]','[value-3]','[value-4]','[value-5]','[value-6]')";
-
 if ($conn){
+    $nombre_completo = $_POST['nombre_completo']; 
+    $edad = $_POST['edad'];  
+    $dni = $_POST['dni'];
+    $email = $_POST['email'];
+    $nombreArchivo = $_FILES['archivo']['name'];
+    $ruta = "images/";
+    $nombre = trim($nombreArchivo);
+    $nombre = mb_ereg_replace(" ","",$nombre);
+    $upload = $ruta . $nombre;
+
+    move_uploaded_file($_FILES['archivo']['tmp_name'], $upload);
+
+    $sql = "INSERT INTO usuario(id, nombre_completo, edad, dni,     email, archivo) VALUES (NULL,'$nombre_completo','$edad','$dni', '$email','$upload')";
+
     mysqli_query($conn,$sql);
+
+    echo $upload;
+
+}else{
+
 }
-
-
 ?>
